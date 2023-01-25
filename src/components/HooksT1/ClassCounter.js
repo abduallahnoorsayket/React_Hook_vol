4e813ed1f6a1677b00e213844ed5b807568725c3
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2023-01-25 14:59:52 +0600

    Hook counter added

---
+++ src/components/HooksT1/ClassCounter.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class ClassCounter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>count {this.state.counter}</button>
+      </div>
+    );
+  }
+}
+
+export default ClassCounter;

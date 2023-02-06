275e757c6c9aced9e6a2c45b64ff138f8cbafd5a
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2023-02-06 17:20:06 +0600

    focus input minor update

--- src/components/UseRef/FocusInput.js
+++ src/components/UseRef/FocusInput.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function FocusInput() {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
   return (
     <div>
-      <input type="text"></input>
+      <input ref={inputRef} type="text"></input>
     </div>
   );
 }
